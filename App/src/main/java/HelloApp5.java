5185d09d86bd79d217517539bb1cd34de8614c83
Yaazhini0 <yaazhini.n@gmail.com>
2026-04-06 09:58:29 +0530

    HelloApp

---
+++ App/src/main/java/HelloApp5.java
@@ -0,0 +1,14 @@
+public class HelloApp5{
+    public static void main(String[] args) {
+        // Check if arguments are provided
+        if (args.length > 0) {
+            // Enhanced for loop to greet each argument
+            for (String name : args) {
+                System.out.println("Hello, " + name + "!");
+            }
+        } else {
+            // Default greeting when no arguments are given
+            System.out.println("Hello, World!");
+        }
+    }
+}
